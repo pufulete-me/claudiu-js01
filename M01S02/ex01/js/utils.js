604430d55c800a695dbb7a7a926e8535f4fbd039
pufulete-me <60327146+pufulete-me@users.noreply.github.com>
2020-05-19 22:13:01 +0300

    js1

---
+++ M01S02/ex01/js/utils.js
@@ -0,0 +1,3 @@
+console.log(45 * 82);
+console.log(Math.pow((13 + 41), 2));
+console.log(Math.pow(15 / 2, 2) * Math.PI);
---
+++ M01S02/ex01/quads.js
@@ -0,0 +1,3 @@
+console.log(Math.pow(16, 2));
+console.log(20 * 12);
+console.log((15 + 38) * 2);
