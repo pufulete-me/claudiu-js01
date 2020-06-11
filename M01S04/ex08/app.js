d9456fe656ca9513a0cede14bc8de435576e13e5
pufulete-me <60327146+pufulete-me@users.noreply.github.com>
2020-06-11 22:00:22 +0300

    S05

--- M01S04/ex08/app.js
+++ M01S04/ex08/app.js
@@ -20,11 +20,32 @@ while (j <= 67) {
 }
 
 console.warn('Modifica exemplul astfel incat bucla sa numere de la 1 la 66 (dar nu modifica numarul 67).');
+console.warn('Folosind keywordul break opreste bucla atunci cand numarul este egal cu 12 (dar afiseaza-l).');
 
 var k = 1;
 
 while (k < 67) {
   console.log(k);
 
+  if (k === 12) {
+    break;
+  }
+
   k++;
 }
+
+console.warn('Folosind keywordul continue afiseaza doar numerele pare intre 1 si 32 (inclusiv ) (bucla noua).');
+
+var z = 0;
+
+while (z <= 32) {
+  z++;
+
+  if (z % 2 !== 0) {
+    console.log(z);
+    continue;
+  }
+
+}
+
+
--- M01S04/ex11/app.js
+++ M01S04/ex11/app.js
@@ -64,7 +64,55 @@ friendsKeysNames.forEach(function (friendKey, i, friends) {
 console.log(message);
 
 console.warn('Afiseaza propozitia: “Prietenii mei sunt Larry Steven si Carol.” folosind Object.keys()');
-message = 'Prietenii mei sunt';
+message = 'Prietenii mei sunt ';
 
+friendsKeysNames = Object.keys(person.friends);
+friendsKeysNames.forEach(function (friendKey, i, friends) {
+  var friendsLength = friends.length;
+  var friend = person.friends[friendKey];
+  var separator = ', ';
 
+  message += friend.name;
 
+  if (i === friendsLength - 1) {
+    separator = '.';
+  }
+
+  if (i === friendsLength - 2) {
+    separator = ' si ';
+  }
+
+  message += separator;
+});
+
+console.log(message);
+
+console.warn('Folosind bucla, afiseaza mai multe propozitii (cate una per console.log()) care sa afiseze: “Larry este mai mare|mic decat Dragos.” etc…');
+
+friendsKeysNames = Object.keys(person.friends);
+friendsKeysNames.forEach(function (friendKey) {
+  message = '';
+  var friend = person.friends[friendKey];
+
+  if (person.age - friend.age > 0) {
+    message = `${friend.name} este mai mic decat ${person.name}.`;
+  } else if (person.age - friend.age < 0) {
+    message = `${friend.name} este mai mare decat ${person.name}.`;
+  } else {
+    message = `${friend.name} este de aceeasi varsta cu ${person.name}.`;
+  }
+  console.log(message);
+});
+
+console.warn('Folosind Object.keys() pe proprietatea skills, afiseaza abilitatile persoanei.');
+Object.keys(person.skills).forEach(function (skillName, i, skills) {
+  console.log(skillName.toLowerCase());
+  // .toLowerCase() pentru lowercase --
+});
+
+console.warn('Prin aceeasi metoda, afiseaza o lista cu numele complet al prietenilor. ');
+// friendKeyNames could be used, it is already declared.
+Object.keys(person.friends).forEach(function (friendKey) {
+  var friend = person.friends[friendKey];
+  console.log(`${friend.name} ${friend.surname}.`);
+});
