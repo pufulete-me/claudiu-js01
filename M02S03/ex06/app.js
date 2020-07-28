879950f442909146d6edf1cf708642dfb2bf91e5
pufulete-me <60327146+pufulete-me@users.noreply.github.com>
2020-07-28 17:38:49 +0300

    sync 538-no idea

---
+++ M02S03/ex06/app.js
@@ -0,0 +1,25 @@
+let buttonElem = document.getElementsByClassName('btn')[0];
+let displayElements = document.getElementsByClassName('display');
+let message = `Butonul a fost apasat in aceasta rezolutie`;
+
+buttonElem.addEventListener('click', () => {
+  let windowWidth = window.innerWidth;
+  let targetClassName = 'lo-res';
+
+  if (windowWidth >= 650) {
+    targetClassName = 'mid-res';
+  }
+
+  if (windowWidth >=1001) {
+    targetClassName = 'hi-res';
+  }
+
+  for (let i = 0; i < displayElements.length; i++) {
+    if(displayElements[i].classList.contains(targetClassName)) {
+      let p = document.createElement('p');
+      p.innerText = message;
+      displayElements[i].append(p);
+    }
+  } 
+});
+
