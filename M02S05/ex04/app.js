baf30315fc42f8ad5f9e4c8fbef756a6b41fd0fe
pufulete-me <60327146+pufulete-me@users.noreply.github.com>
2020-08-09 16:43:53 +0300

    curs 10

---
+++ M02S05/ex04/app.js
@@ -0,0 +1,4 @@
+// equivalent to document.addEventListener('DOMContentLoaded') in jS
+$(document).ready(() => {
+  $('.extra-cat').fancybox();
+});
---
+++ M02S05/ex05/app.js
@@ -0,0 +1,10 @@
+$(document).ready(() => {
+  $('.my-images').slick({
+    dots: true,
+    centerMode: true,
+    centerPadding: '100px',
+    slidesToShow: 3,
+  });
+
+  let tobii = new Tobii();
+});
